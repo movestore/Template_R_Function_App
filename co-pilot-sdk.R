7296fca49636db4b4c402e5e3083ed4d800ea88d
Clemens Hahn <clemens@couchbits.com>
2022-09-14 15:17:39 +0200

    simplify the SDK

--- co-pilot-sdk.R
+++ co-pilot-sdk.R
@@ -23,37 +23,17 @@ options(error = function() traceback(3))
 # Add your arguments of your r-function here
 args[["year"]] = 2014 
 
-#################################################################
-############################# main ##############################
-#################################################################
-
 # tie everything together
 # the following files will NOT bundled into the final app - they are just helpers for the SDK
 source("src/common/logger.R")
 source("src/io/app_files.R")
 source("src/io/io_handler.R")
 source("src/io/rds.R")
+source("src/moveapps.R")
 # this file comes from the app developer and will be bundled into the final app
 source("RFunction.R")
 
 # set your environment
 Sys.setenv(SOURCE_FILE = inputFileName, OUTPUT_FILE = outputFile, ERROR_FILE="./data/output/error.log")
-tryCatch(
-{
-  Sys.setenv(tz="UTC")
 
-  data <- readInput(sourceFile())
-  if (!is.null(data)) {
-    args[["data"]] <- data
-  }
-
-  result <- do.call(rFunction, args)
-  storeResult(result, outputFile())
-},
-error = function(e)
-{
-  # error handler picks up where error was generated
-  print(paste("ERROR: ", e))
-  storeToFile(e, errorFile())
-  stop(e) # re-throw the exception
-})
+simulateMoveAppsRun(args)
