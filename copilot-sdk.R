aadcd6a2e86085d64a38aeccaf102ce06e54a145
Clemens Hahn <clemens@couchbits.com>
2022-09-14 14:29:49 +0200

    organize the SDK

--- copilot-sdk.R
+++ copilot-sdk.R
@@ -1,13 +1,9 @@
-library("jsonlite")
-library("move")
-source("logger.R")
-source("app-files.R")
-source("RFunction.R")
-
-Sys.setenv(tz="UTC")
-
-inputFileName = "input1_pigeons.rds" ## Provided testing datasets: "input1_pigeons.rds", "input2_geese.rds", "input3_stork.rds", "input4_goat.rds"  ## for own data: file saved as a .rds containing a object of class MoveStack
-outputFileName = "output.rds" ## optionally change the output file name
+## Provided testing datasets in `data/raw`: 
+## "input1_pigeons.rds", "input2_geese.rds", "input3_stork.rds", "input4_goat.rds"  
+## for own data: file saved as a .rds containing a object of class MoveStack
+inputFileName = "./data/raw/input1_pigeons.rds" 
+## optionally change the output file name
+outputFileName = "./data/output/output.rds" 
 
 args <- list()
 
@@ -21,67 +17,40 @@ args <- list()
 #    args[["username"]] = "any-username"
 #    args[["password"]] = "any-password"
 
-# Add your arguments of your r function here
+# Add your arguments of your r-function here
 args[["year"]] = 2014 
 
-#####################################################################
-#####################################################################
-## DO NOT MODIFY THE CODE BELOW!  
-## All the code below simulates the MoveApps environment to enable ##
-## testing an App locally. Hit "Source" (top right of this panel)  ##
-## to run this script.                                             ##
-## DO NOT MODIFY THE CODE BELOW!   
-#####################################################################
-#####################################################################
+#################################################################
+############################# main ##############################
+#################################################################
 
-readInput <- function(sourceFile) {
-  input <- NULL
-  if(!is.null(sourceFile) && sourceFile != "") {
-    if (file.info(sourceFile)$size == 0) {
-      # handle the special `null`-input
-        logger.warn("The App has received invalid input! It cannot process NULL-input. Aborting..")
-        stop("The App has received invalid input! It cannot process NULL-input. Check the output of the preceding App or adjust the datasource configuration.")
-    }
-    logger.debug("Loading file from %s", sourceFile)
-    input <- tryCatch({
-        # 1: try to read input as move RDS file
-        readRDS(file = sourceFile)
-      },
-      error = function(readRdsError) {
-        tryCatch({
-          # 2 (fallback): try to read input as move CSV file
-          move(sourceFile, removeDuplicatedTimestamps=TRUE)
-        },
-        error = function(readCsvError) {
-          # collect errors for report and throw custom error
-          stop(paste(sourceFile, " -> readRDS(sourceFile): ", readRdsError, "move(sourceFile): ", readCsvError, sep = ""))
-        })
-      })
-  } else {
-    logger.debug("Skip loading: no source File")
-  }
+# tie everything together
+# the following files will NOT bundled into the final app - they are just helpers for the SDK
+source("src/common/logger.R")
+source("src/io/app_files.R")
+source("src/io/io_handler.R")
+source("src/io/rds.R")
+# this file comes from the app developer and will be bundled into the final app
+source("RFunction.R")
 
-  input
-}
-
+# set your environment
+Sys.setenv(SOURCE_FILE = inputFileName, OUTPUT_FILE = outputFile, ERROR_FILE="./data/output/error.log")
 tryCatch(
 {
-  inputData <- readInput(inputFileName)
-  # Add the data parameter if input data is available
-  if (!is.null(inputData)) {
-    args[["data"]] <- inputData
+  Sys.setenv(tz="UTC")
+
+  data <- readInput(sourceFile())
+  if (!is.null(data)) {
+    args[["data"]] <- data
   }
 
   result <- do.call(rFunction, args)
-
-  if(!is.null(outputFileName) && outputFileName != "" && !is.null(result)) {
-    logger.info(paste("Storing file to '", outputFileName, "'", sep = ""))
-    saveRDS(result, file = outputFileName)
-  } else {
-    logger.warn("Skip store result: no output File or result is missing.")
-  }
+  storeResult(result, outputFile())
 },
-error = function(e) {
-  logger.error(paste("ERROR:", e))
+error = function(e)
+{
+  # error handler picks up where error was generated
+  print(paste("ERROR: ", e))
+  storeToFile(e, errorFile())
   stop(e) # re-throw the exception
 })
