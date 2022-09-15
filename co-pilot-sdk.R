9809a934d9df09b5e1459a640f4ebe43385f9ab7
Clemens Hahn <clemens@couchbits.com>
2022-09-15 12:55:11 +0200

    housekeeping

--- co-pilot-sdk.R
+++ co-pilot-sdk.R
@@ -5,9 +5,6 @@ inputFileName = "./data/raw/input2_geese.rds"
 ## optionally change the output file name
 outputFileName = "./data/output/output.rds" 
 
-# enable better error reporting during development
-options(error = function() traceback(3))
-
 #################################################################
 ########################### Arguments ###########################
 # The data parameter will be added automatically if input data is available
