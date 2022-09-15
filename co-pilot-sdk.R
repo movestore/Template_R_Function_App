70694672e92e6e1652f66e9cc9ca093ab6487b8f
Clemens Hahn <clemens@couchbits.com>
2022-09-15 08:44:16 +0200

    housekeeping

--- co-pilot-sdk.R
+++ co-pilot-sdk.R
@@ -1,11 +1,9 @@
-## Provided testing datasets in `data/raw`: 
+## Provided testing datasets in `./data/raw`: 
 ## "input1_pigeons.rds", "input2_geese.rds", "input3_stork.rds", "input4_goat.rds"  
 ## for own data: file saved as a .rds containing a object of class MoveStack
 inputFileName = "./data/raw/input1_pigeons.rds" 
 ## optionally change the output file name
 outputFileName = "./data/output/output.rds" 
-
-args <- list()
 
 # enable better error reporting during development
 options(error = function() traceback(3))
@@ -15,11 +13,12 @@ options(error = function() traceback(3))
 # The data parameter will be added automatically if input data is available
 # The name of the field in the vector must be exactly the same as in the r function signature
 # Example:
-# rFunction = function(username, password)
+# rFunction = function(username, department)
 # The parameter must look like:
 #    args[["username"]] = "any-username"
-#    args[["password"]] = "any-password"
+#    args[["department"]] = "any-department"
 
+args <- list()
 # Add your arguments of your r-function here
 args[["year"]] = 2014 
 
@@ -33,7 +32,13 @@ source("src/moveapps.R")
 # this file comes from the app developer and will be bundled into the final app
 source("RFunction.R")
 
-# set your environment
-Sys.setenv(SOURCE_FILE = inputFileName, OUTPUT_FILE = outputFile, ERROR_FILE="./data/output/error.log", APP_ARTIFACTS_DIR ="./data/output/artifacts")
+# setup your environment
+Sys.setenv(
+    SOURCE_FILE = inputFileName, 
+    OUTPUT_FILE = outputFileName, 
+    ERROR_FILE="./data/output/error.log", 
+    APP_ARTIFACTS_DIR ="./data/output/artifacts"
+)
 
+# simulate running your app on MoveApps
 simulateMoveAppsRun(args)
