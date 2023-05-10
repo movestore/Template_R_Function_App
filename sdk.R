74fccef9652305ad4035403b689d3f2cf64ab27c
Clemens Hahn <clemens@couchbits.com>
2023-05-10 13:18:49 +0200

    source in moveapps file

--- sdk.R
+++ sdk.R
@@ -4,10 +4,6 @@ library(dotenv)
 # Per default your environment is defined in `/.env`
 load_dot_env()
 
-# `./RFunction.R` is the home of your app code
-# It is the only file which will be bundled into the final app on MoveApps
-source("RFunction.R")
-
 # Lets simulate running your app on MoveApps
 source("src/common/runtime_configuration.R")
 # This will parse a JSON file containing the concrete configuration of
