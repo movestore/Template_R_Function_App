e4100a82aa8e677021b56e61f2373396f6dd6af6
Clemens Hahn <clemens@couchbits.com>
2023-06-20 08:21:11 +0200

    code style

--- RFunction.R
+++ RFunction.R
@@ -3,8 +3,9 @@ library('lubridate')
 
 ## The parameter "data" is reserved for the data object passed on from the previous app
 
-## to display messages to the user in the log file of the App in MoveApps one can use the function from the logger.R file: 
-# logger.fatal(), logger.error(), logger.warn(), logger.info(), logger.debug(), logger.trace()
+# to display messages to the user in the log file of the App in MoveApps
+# one can use the function from the logger.R file:
+# logger.info(). Available levels are error(), warn(), info(), debug(), trace()
 
 rFunction = function(data, sdk, year, ...) {
   logger.info(paste("Welcome to the", sdk))
