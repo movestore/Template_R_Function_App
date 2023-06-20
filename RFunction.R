d0c7b46c24f46935ed7e1fe6cf0c1e93ca9d0795
Clemens Hahn <clemens@couchbits.com>
2023-06-20 11:20:09 +0200

    add showcase about local files

--- RFunction.R
+++ RFunction.R
@@ -7,6 +7,7 @@ library('lubridate')
 # one can use the function from the logger.R file:
 # logger.fatal(), logger.error(), logger.warn(), logger.info(), logger.debug(), logger.trace()
 
+# Showcase injecting app setting (parameter `year`)
 rFunction = function(data, sdk, year, ...) {
   logger.info(paste("Welcome to the", sdk))
   result <- if (any(lubridate::year(data@timestamps) == year)) { 
@@ -15,6 +16,8 @@ rFunction = function(data, sdk, year, ...) {
     NULL
   }
   if (!is.null(result)) {
+    # Showcase creating an app artifact. 
+    # This artifact can be downloaded by the workflow user on Moveapps.
     artifact <- appArtifactPath("plot.png")
     logger.info(paste("plotting to artifact:", artifact))
     png(artifact)
@@ -23,6 +26,12 @@ rFunction = function(data, sdk, year, ...) {
   } else {
     logger.warn("nothing to plot")
   }
+  # Showcase to access a file ('auxiliary files') that is 
+  # a) provided by the app-developer and 
+  # b) can be overridden by the workflow user.
+  fileName <- paste0(getAppFilePath("yourLocalFileSettingId"), "sample.txt")
+  logger.info(readChar(fileName, file.info(fileName)$size))
+
   # provide my result to the next app in the MoveApps workflow
   return(result)
 }
