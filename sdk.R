b5ba3ca66196b3a593c88c97ccb5480d729a1287
Clemens Hahn <clemens@couchbits.com>
2025-09-30 12:10:55 +0200

    update renv and moveapps lib

--- sdk.R
+++ sdk.R
@@ -13,6 +13,7 @@ moveapps::clearRecentOutput()
 args <- moveapps::configuration()
 
 library("moveapps")
+Sys.setenv(tz="UTC")
 # `./RFunction.R` is the home of your app code
 # It is the only file which will be bundled into the final app on MoveApps
 source("RFunction.R")
